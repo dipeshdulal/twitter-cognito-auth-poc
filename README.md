cad5173157ae3339b7b8a553a3674d11f8219b26
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2024-02-12 18:28:54 +0545

    feat: initial commit for twitter oauth shim

---
+++ README.md
@@ -0,0 +1,31 @@
+### POC for Cognito with Twitter OAuth2.0
+
+Cognito doesn't officially support twitter OAuth2.0.
+There is a way to add custom IDP using OIDC but twitter doesn't support OIDC.
+This adds some way to transform the input data from cognito and proxy request to twitter.
+
+JWKS endpoints are not called by cognito when using PKCS method of authentication.
+Looks like any random values can be added in the cognito endpoint configuration.
+
+#### Running
+
+- copy `.env.example` to `.env` and update required values.
+- run the server, open ngrok or any tunneling software.
+- setup twitter oauth application to redirect to the tunnel address.
+- setup cognito idp to use the urls from the tunnel address.
+
+#### Working;
+
+- Cognito requests login to this endpoint when doing the sign in operation.
+- The endpoint adds necessary parameters and forwards the request to twitter.
+- Twitter redirects back to our endpoint and we redirect back to cognito.
+- Cognito calls `/token` and `/userinfo` endpoint for creating/updating users when logging in.
+
+#### Endpoints
+
+| Points       | Description                                                                                                                                              |
+| ------------ | -------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| `/authorize` | Authorize calls the twitter oauth entrypoint that shows user consent screen.                                                                             |
+| `/callback`  | Callback receives code and state from twitter server and processes accordingly, redirecting to aws cognito                                               |
+| `/token`     | Responsible for getting code from cognito and then calling the twitter service to exchange code with auth token. Response is sent back to cognito as is  |
+| `/userinfo`  | Cognito calls to map the idp attributes with the cognito attributes. The call is made using the bearer token from twitter server in the previous request |
---
+++ main.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+// map cognito state with twitter state
+var stateCache = map[string]string{}
+
+// map generated state with code challenge
+var stateCodeChallenge = map[string]string{}
+
+// map code with code challenge
+var codeCodeChallenge = map[string]string{}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+    b := make([]rune, n)
+    for i := range b {
+        b[i] = letterRunes[rand.Intn(len(letterRunes))]
+    }
+    return string(b)
+}
+
+func main() {
+
+  godotenv.Load()
+
+  cognitoUrl := os.Getenv("COGNITO_AUTH_URL")
+  callbackURL := os.Getenv("CALLBACK_URL")
+
+	engine := gin.Default()
+
+  engine.GET("/authorize", func (c *gin.Context) {
+    query := c.Request.URL.Query()
+    twitterUrl := "https://twitter.com/i/oauth2/authorize" 
+
+    
+    // urlencode cognito url 
+    query.Del("redirect_uri")
+    query.Set("redirect_uri", callbackURL)
+    query.Del("scope")
+    query.Set("scope", "users.read offline.access tweet.read")
+
+    codeChallenge := RandStringRunes(30)
+    query.Set("code_challenge", codeChallenge)
+    query.Set("code_challenge_method", "plain")
+
+    state := query.Get("state")
+    // generate random state
+    
+    randomState := RandStringRunes(10)
+    stateCache[randomState] = state 
+    stateCodeChallenge[randomState] = codeChallenge
+
+    query.Del("state")
+    query.Set("state", randomState)
+
+    // redirect to twitter url with the query params 
+    c.Redirect(http.StatusFound, twitterUrl + "?" + query.Encode())
+  })
+
+  engine.GET("/callback", func (c *gin.Context) {
+    query := c.Request.URL.Query() 
+    state := query.Get("state")
+    query.Del("state")
+    query.Del("client_secret")
+
+    
+    code := query.Get("code")
+    codeChallenge := stateCodeChallenge[state]
+    codeCodeChallenge[code] = codeChallenge
+    
+    oldState, ok := stateCache[state]
+    if !ok {
+      c.JSON(http.StatusBadRequest, gin.H{"error": "state not found"})
+      return
+    }
+    query.Add("state", oldState)
+
+    c.Redirect(http.StatusFound, cognitoUrl + "?" + query.Encode())
+  })
+
+  engine.POST("/token", func (c *gin.Context) {
+    // parse form params 
+    if err := c.Request.ParseForm(); err != nil {
+      c.JSON(http.StatusBadRequest, gin.H{"error": "invalid form"})
+      return
+    }
+
+    // get the form params
+    form := c.Request.PostForm
+
+    form.Del("redirect_uri")
+    form.Set("redirect_uri", callbackURL)
+    form.Del("client_secret")
+
+    code := form.Get("code")
+    codeVerifier := codeCodeChallenge[code] 
+
+    form.Set("code_verifier", codeVerifier)
+
+    // make a request to twitter to get the access token
+    body := form.Encode()
+    log.Println(body)
+    strReader := strings.NewReader(body)
+
+    request, err := http.NewRequest("POST", "https://api.twitter.com/2/oauth2/token", strReader)
+    if err != nil { 
+      c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
+      return
+    }
+
+    request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+    client := &http.Client{}
+    response, err := client.Do(request)
+    if err != nil {
+      c.JSON(http.StatusBadRequest, gin.H{"error": "invalid response"})
+      return
+    }
+
+    defer response.Body.Close()
+
+    // return response as is 
+    c.Header("Content-Type", "application/json")
+    c.Status(response.StatusCode)
+    io.Copy(c.Writer, response.Body)
+  })
+
+  engine.GET("/userinfo", func (c *gin.Context) {
+    // get token from the request 
+    token := c.Request.Header.Get("Authorization")
+    if token == "" {
+      c.JSON(http.StatusBadRequest, gin.H{"error": "invalid token"})
+      return
+    }
+
+    // make a request to twitter to get the user info
+    request, err := http.NewRequest("GET", "https://api.twitter.com/2/users/me", nil)
+    if err != nil {
+      c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
+      return
+    }
+
+    request.Header.Set("Authorization", token)
+    
+    client := &http.Client{}
+    response, err := client.Do(request)
+    if err != nil {
+      c.JSON(http.StatusBadRequest, gin.H{"error": "invalid response"})
+      return
+    }
+
+    defer response.Body.Close()
+   
+    // read body as map[string]interface{}
+    var body map[string]interface{}
+    strOp, err := io.ReadAll(response.Body)
+    if err != nil {
+      c.JSON(http.StatusBadRequest, gin.H{"error": "invalid response"})
+      return
+    }
+
+    if err := json.Unmarshal(strOp, &body); err != nil {
+      c.JSON(http.StatusBadRequest, gin.H{"error": "invalid response"})
+      return
+    }
+
+    data := body["data"].(map[string]interface{})
+
+    data["sub"] = data["id"]
+    data["email"] = "nilkantha.dipesh+twitter-1@gmail.com"
+    c.JSON(http.StatusOK, data)
+  })
+
+  engine.Run(":8080")
+}
